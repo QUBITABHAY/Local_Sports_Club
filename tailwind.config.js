22351420e8b69fc5a3c35f1f7f59ee768f83e919
QUBITABHAY <codingcontestabhay@gmail.com>
2024-11-29 21:48:33 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "about.html",
+    "faq.html",
+    "gallary.html",
+    "rule.html",
+    "sports.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
