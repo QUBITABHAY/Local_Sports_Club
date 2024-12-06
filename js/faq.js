9275bf8c1fa87b1c7ea0f0c73722c0cd8059a2f1
QUBITABHAY <codingcontestabhay@gmail.com>
2024-12-06 21:46:49 +0530

    faq page

---
+++ js/faq.js
@@ -0,0 +1,51 @@
+const faq1 = document.getElementById("faq1")
+const faq2 = document.getElementById("faq2")
+const faq3 = document.getElementById("faq3")
+const faq4 = document.getElementById("faq4")
+const faq5 = document.getElementById("faq5")
+const faq6 = document.getElementById("faq6")
+const faq7 = document.getElementById("faq7")
+const faq8 = document.getElementById("faq8")
+const faq9 = document.getElementById("faq9")
+const faq10 = document.getElementById("faq10")
+
+faq1.addEventListener("click", function (e) {
+
+    faq1.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq2.addEventListener("click", function (e) {
+    faq2.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq3.addEventListener("click", function (e) {
+    faq3.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq4.addEventListener("click", function (e) {
+    faq4.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq5.addEventListener("click", function (e) {
+    faq5.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq6.addEventListener("click", function (e) {
+    faq6.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq7.addEventListener("click", function (e) {
+    faq7.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq8.addEventListener("click", function (e) {
+    faq8.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq9.addEventListener("click", function (e) {
+    faq9.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
+
+faq10.addEventListener("click", function (e) {
+    faq10.innerHTML = `<h3>Q1</h3><p>ansi</p>`
+})
